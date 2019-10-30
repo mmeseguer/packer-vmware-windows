1930d5cebef445bacc3869b5d1a191469b4fe268
Marc <mmeseguer87@gmail.com>
2019-10-30 13:13:52 +0100

    Cleaning

--- setup/setup.ps1
+++ setup/setup.ps1
@@ -11,5 +11,4 @@ winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 winrm set winrm/config/service/auth '@{Basic="true"}'
 
 # Reset auto logon count
-# https://docs.microsoft.com/en-us/windows-hardware/customize/desktop/unattend/microsoft-windows-shell-setup-autologon-logoncount#logoncount-known-issue
 Set-ItemProperty -Path 'HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon' -Name AutoLogonCount -Value 0
